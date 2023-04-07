a8d65b1a60367ca7845358bf24fc124585cba6bf
Ludwig <119101068+Ludwig-Murimi@users.noreply.github.com>
2023-04-07 21:23:38 +0300

    Feat:Creation of the navbar

--- portfolio/src/components/Sidebar/index.js
+++ portfolio/src/components/Sidebar/index.js
@@ -1,8 +1,9 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo-sub.png'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faEnvelope, faUser, faCode} from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => (
     <div className="nav-bar">
@@ -10,6 +11,21 @@ const Sidebar = () => (
             <img src={LogoS} alt="logo" />
             <img className='sub-logo' src={LogoSubtitle} alt="Ludwig" />
         </Link>
+        <nav>
+            <NavLink exact="true" activeclassname="active" to="/" >
+                <FontAwesomeIcon icon={faHome}color='#4d4d4e' />
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="about-link" to="/about" >
+                <FontAwesomeIcon icon={faUser}color='#4d4d4e' />
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="project-link" to="/project" >
+                <FontAwesomeIcon icon={faCode}color='#4d4d4e' />
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="contact-link"to="/contact" >
+                <FontAwesomeIcon icon={faEnvelope}color='#4d4d4e' />
+            </NavLink>
+
+        </nav>
     </div>
 )
 
