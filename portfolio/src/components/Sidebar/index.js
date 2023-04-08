927b9aef76514ffa33f6e6e0fd1f5c7ebf225edd
Ludwig <119101068+Ludwig-Murimi@users.noreply.github.com>
2023-04-08 03:28:13 +0300

    feat:modification of the sidenavbar including my various platforms

--- portfolio/src/components/Sidebar/index.js
+++ portfolio/src/components/Sidebar/index.js
@@ -3,7 +3,8 @@ import './index.scss'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo-sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faEnvelope, faUser, faCode} from '@fortawesome/free-solid-svg-icons'
+import { faHome, faEnvelope, faUser, faCode} from '@fortawesome/free-solid-svg-icons';
+import {faLinkedin, faTwitter,faGithub, faSkype} from '@fortawesome/free-brands-svg-icons';
 
 const Sidebar = () => (
     <div className="nav-bar">
@@ -24,8 +25,29 @@ const Sidebar = () => (
             <NavLink exact="true" activeclassname="active" className="contact-link"to="/contact" >
                 <FontAwesomeIcon icon={faEnvelope}color='#4d4d4e' />
             </NavLink>
-
         </nav>
+        <ul>
+            <li>
+                <a target='_blank' rel='noreferrer' href="https://www.linkedin.com/in/ludwig-murimi/">
+                  <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a target='_blank' rel='noreferrer' href="https://github.com/lu-dwig">
+                  <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a target='_blank' rel='noreferrer' href="https://twitter.com/LudwigMunyi">
+                  <FontAwesomeIcon icon={faTwitter} color="#4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a target='_blank' rel='noreferrer' href="skype:live:.cid.d94cac83e11fe430">
+                  <FontAwesomeIcon icon={faSkype} color="#4d4d4e" />
+                </a>
+            </li>
+        </ul>
     </div>
 )
 
