ae47afcdee58132b94a635468afe3cb6506098f8
Ludwig <119101068+Ludwig-Murimi@users.noreply.github.com>
2023-04-14 14:55:16 +0300

    fix:cretaion fo the animated letters and modification of the homepage

--- portfolio/src/components/Home/index.js
+++ portfolio/src/components/Home/index.js
@@ -1,17 +1,37 @@
-import { Link } from 'react-router-dom';
-import LogoTitle from '../../assets/images/logo-s.png';
-import './index.scss';
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import LogoTitle from '../../assets/images/logo-s.png'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
 
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+
+    const nameArray = [ 'u', 'd', 'w', 'i', 'g']
+    const jobArray = [ 'A', ' ', 'S', 'o','f','t','w','a','r','e', ' ','D','e','v','e','l','o','p','e','r', '.',]
+
+    // useEffect(() =>{
+        // return setTimeout(() => {
+            // setLetterClass('text-animate-hover')
+        // },4000)
+    // }, [])
 
     return (
         <>
             <div className="container home-page">
                 <div className="text-zone">
-                    <h1>Hi,<br/> I'm  <img src={LogoTitle} alt='developer'/>
-                        udwig Murimi
+                    <h1>
+                        <span className={letterClass}>H</span>
+                        <span className={`${letterClass} _12`}>i</span>
+                        <br/> 
+                        <span className={`${letterClass} _13`}>I</span>
+                        <span className={`${letterClass} _14`}>'m</span>
+                    
+                        <img src={LogoTitle} alt='developer'/>
+                        <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15}/>
                         <br/>
-                        A Software Developer
+                        <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={22}/>
+
                     </h1>
                     <h2>Full-Stack Developer | Javascript and Python Expert</h2>
                     <Link to="/contact" className='flat-button'>Contact Me</Link>
