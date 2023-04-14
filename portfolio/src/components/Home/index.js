ec3192c022f8313fa04a04586898d046f168d81a
Ludwig <119101068+Ludwig-Murimi@users.noreply.github.com>
2023-04-14 08:11:35 +0300

    perf:fixation of the home page main components and styling

--- portfolio/src/components/Home/index.js
+++ portfolio/src/components/Home/index.js
@@ -8,11 +8,10 @@ const Home = () => {
         <>
             <div className="container home-page">
                 <div className="text-zone">
-                    <h1>Hi,<br/> I'm
-                        <img src={LogoTitle} alt='developer'/>
+                    <h1>Hi,<br/> I'm  <img src={LogoTitle} alt='developer'/>
                         udwig Murimi
                         <br/>
-                        Web Developer
+                        A Software Developer
                     </h1>
                     <h2>Full-Stack Developer | Javascript and Python Expert</h2>
                     <Link to="/contact" className='flat-button'>Contact Me</Link>
